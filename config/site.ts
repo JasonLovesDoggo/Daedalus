36e2c2024c97f7e6c18f06f54f8bcf040ce3ed7d
araf821 <araf.ahmed200@gmail.com>
2024-08-28 23:26:10 -0400

    chore: init auth route group, site config and types

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+import { SidebarLink } from "@/types";
+
+export const sidebarLinks: SidebarLink[] = [
+  {
+    label: "Dashboard",
+    href: "/",
+  },
+];
+
+// No need for an over-the-top site config for SEO purposes as almost the entire site is password protected and cannot be access by bots or scrapers anyway
+export const siteConfig = {
+  title: "Hack Canada",
+  description:
+    "Canada's largest hackathon with over 5000+ hackers and sponsors.",
+};
---
+++ types/index.d.ts
@@ -0,0 +1,4 @@
+export type SidebarLink = {
+  label: string;
+  href: string;
+};
