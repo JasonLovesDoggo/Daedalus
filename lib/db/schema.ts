430833c7e98587a367439b115994e6f54fa69593
araf821 <araf.ahmed200@gmail.com>
2024-09-07 22:50:02 -0400

    fix: drizzle kit downgrade to support db:push

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -2,7 +2,6 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  username: text("username") /*.unique()*/
-    .notNull(),
+  username: text("username").notNull(),
   email: text("email").unique().notNull(),
 });
