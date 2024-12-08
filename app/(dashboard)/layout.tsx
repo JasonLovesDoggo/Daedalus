343bdfaa78f6f9efd1a0059d03ff5d78b7f3c1bc
araf821 <araf.ahmed200@gmail.com>
2024-12-08 06:17:04 -0500

    feat: initial dashboard layout structure

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,13 +5,14 @@ type Props = {
 const DashboardLayout = ({ children }: Props) => {
   return (
     <div className="flex-1">
-      <nav className="fixed inset-x-0 top-0 z-30 flex h-20 items-center justify-center border-b-2">
+      {/* Uncomment if a navbar is needed */}
+      {/* <nav className="fixed inset-x-0 top-0 z-30 flex h-20 items-center justify-center border-b-2">
         Navbar
-      </nav>
+      </nav> */}
       <div className="fixed inset-y-0 z-30 hidden w-72 border-r bg-background lg:flex">
         Sidebar
       </div>
-      <main className="pt-20 lg:pl-72">{children}</main>
+      <main className="h-svh lg:ml-72">{children}</main>
     </div>
   );
 };
