7fcbe35c73564d2b39a80e69aeb37090ed1bf510
araf821 <araf.ahmed200@gmail.com>
2024-12-25 09:18:49 -0500

    chore: improve ApplicationPage layout and add NotFound component for better user experience

--- app/(dashboard)/applications/page.tsx
+++ app/(dashboard)/applications/page.tsx
@@ -1,12 +1,14 @@
 import { applications } from "@/config/applications";
-import { ApplicationCard } from "@/components/dashboard/ApplicationCard";
+import { ApplicationCard } from "@/components/applications/ApplicationCard";
 
 const ApplicationPage = () => {
   return (
     <div className="mx-auto h-full w-full max-w-screen-lg space-y-6 px-4 py-8 md:space-y-8 md:px-8 md:py-12 lg:space-y-10 lg:px-12 lg:py-20 xl:space-y-12 xl:px-16 xl:py-28 2xl:py-32 min-[1850px]:max-w-screen-xl">
       <div className="mb-8 space-y-2">
-        <h1 className="text-3xl font-bold">Manage Your Applications</h1>
-        <p className="text-sm text-textMuted">
+        <div className="w-fit bg-gradient-to-r from-primary via-sky-400 to-primary bg-clip-text text-transparent">
+          <h1 className="text-3xl font-bold">Manage Your Applications</h1>
+        </div>
+        <p className="text-textMuted max-md:text-sm">
           Explore application details and stay updated on deadlines.
         </p>
       </div>
--- components/dashboard/DashboardHeader.tsx
+++ components/dashboard/DashboardHeader.tsx
@@ -6,11 +6,13 @@ export const DashboardHeader = ({ userName }: DashboardHeaderProps) => {
   const firstName = userName?.split(" ")[0] || "User";
 
   return (
-    <div className="space-y-4">
-      <h1 className="text-3xl font-black md:text-4xl xl:text-5xl">
-        Hello, {firstName}.
-      </h1>
-      <p className="text-lg font-light text-gray-500 md:text-xl xl:text-2xl">
+    <div className="space-y-3 overflow-hidden">
+      <div className="w-fit bg-gradient-to-r from-primary via-sky-400 to-primary bg-clip-text text-transparent">
+        <h1 className="text-3xl font-black md:text-4xl xl:text-5xl">
+          Hello, {firstName}.
+        </h1>
+      </div>
+      <p className="text-lg font-light text-gray-500 md:text-xl">
         Welcome back!
       </p>
     </div>
