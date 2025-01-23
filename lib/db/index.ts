47cf21a9a52d4106e7577f8a17448c59d6f87621
araf821 <araf.ahmed200@gmail.com>
2025-01-22 22:46:09 -0500

    chore: migrate to neondb from supabase

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,27 +1,16 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+
+import * as schema from "./schema";
 
 declare global {
-  var _db: ReturnType<typeof drizzle> | undefined;
+  var _db: ReturnType<typeof drizzle<typeof schema>> | undefined;
 }
 
-const connectionString = process.env.DATABASE_URL!;
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
-// Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(connectionString, {
-  prepare: false,
-  max: 1,
-  idle_timeout: 20,
-  connection: {
-    ssl: true,
-  },
-});
-
-export const db = globalThis._db || drizzle(client);
+export const db = globalThis._db || drizzle({ client: pool, schema: schema });
 
 if (process.env.NODE_ENV !== "production") {
   globalThis._db = db;
 }
-
-// Run migrations
-// migrate(db, { migrationsFolder: "drizzle" });
