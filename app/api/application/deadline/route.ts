2e5b8243de7d4767a6f5ac69492be4841bb6c554
Stocker <44980366+StockerMC@users.noreply.github.com>
2025-02-01 13:19:20 -0500

    feat: update application deadline

--- app/api/application/deadline/route.ts
+++ app/api/application/deadline/route.ts
@@ -7,7 +7,7 @@ export const revalidate = 0;
 export async function GET(
   req: NextRequest,
 ): Promise<NextResponse<ApiResponse>> {
-  const hackerApplicationDeadline = new Date("2025-01-31T23:59:59-05:00");
+  const hackerApplicationDeadline = new Date("2025-02-03T23:59:59-05:00");
 
   return NextResponse.json({
     success: true,
