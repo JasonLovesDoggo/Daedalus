351b6fc03ed66ec777d2a3e5c297a8609bd1b08f
araf821 <araf.ahmed200@gmail.com>
2025-02-16 16:26:32 -0500

    chore: update Discord invite link and add Hacker Package URL

--- config/site.ts
+++ config/site.ts
@@ -25,8 +25,9 @@ export const siteConfig = {
 };
 
 // TODO: Add links once they are ready
-export const discordInviteUrl = "https://discord.gg/thbY3eUkuB";
-export const hackerPackageUrl = "";
+export const discordInviteUrl = "https://discord.gg/6sHskEpdpb";
+export const hackerPackageUrl =
+  "https://torpid-tuesday-6d4.notion.site/Hack-Canada-Hacker-Package-1805d88c3a21800198e9e0731d94dc3f";
 
 export const eventDate = new Date("2025-02-21T16:30:00-05:00");
 export const hackerApplicationDeadline = new Date("2025-02-10T23:59:59-05:00");
