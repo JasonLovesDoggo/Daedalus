d93e6755619d5fb31f53fd82967e3a0048580e62
araf821 <araf.ahmed200@gmail.com>
2025-02-23 23:52:39 -0500

    chore: update dashboard to reflect conclusion of Hack Canada 2025

--- components/dashboard/DashboardContent.tsx
+++ components/dashboard/DashboardContent.tsx
@@ -27,7 +27,25 @@ export const DashboardContent = ({ user }: DashboardContentProps) => {
           "flex-col-reverse": user.role === "hacker",
         })}
       >
-        <ApplicationStatus status={user.status} role={user.role} />
+        {/* Application Status commented out post-hackathon */}
+        {/* <ApplicationStatus status={user.status} role={user.role} /> */}
+
+        <div className="relative w-full rounded-md border-2 border-primary/25 p-6 transition-all duration-500 before:absolute before:inset-0 before:-z-10 before:bg-gradient-to-br before:from-primary/20 before:via-info/30 before:to-primaryLight/30 before:opacity-75 md:p-8 xl:p-12">
+          <h2 className="mb-4 text-2xl font-medium text-textPrimary">
+            Hack Canada 2025 Has Concluded
+          </h2>
+          <p className="text-textPrimary/70">
+            {user.role === "hacker" &&
+              "Thank you for participating in Hack Canada 2025! Your projects and enthusiasm made this event truly special. We hope you gained valuable experience, made lasting connections, and will join us again for Hack Canada 2026!"}
+            {user.role === "organizer" ||
+              (user.role === "admin" &&
+                "Congratulations on successfully organizing Hack Canada 2025! Your hard work and dedication made this event possible. Time to start planning for an even bigger and better Hack Canada 2026!")}
+            {user.role === "volunteer" &&
+              "Thank you for volunteering at Hack Canada 2025! Your dedication and hard work helped make this event run smoothly. We truly appreciate your contribution and hope you'll be part of our team again next year!"}
+            {user.role === "unassigned" &&
+              "Thank you for your interest in Hack Canada 2025! While this year's event has concluded, we encourage you to stay tuned for updates about Hack Canada 2026. We'd love to have you join us next year!"}
+          </p>
+        </div>
 
         {/* Profile Section */}
         <div className="grid gap-6 md:gap-8 lg:grid-cols-5 lg:gap-10">
